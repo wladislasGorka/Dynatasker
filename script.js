80d570b082350c15cb4101370f25095348343ce7
wladislasGorka <wladislas.gorka@gmail.com>
2024-11-02 17:33:59 +0100

    Amélioration du tri + appel de fonction printList + btn completeTask

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let taches; // Tableau des taches construit à partir du fichier json
 let currentTaskDisplay = ""; // tache dont la description est visible
+let currentFilter = "Toutes"; // filtre en cours d'utilisation pour l'affichage des tâches
 
 // Récupération des informations dans le .json pour créer le tableau des taches
 async function monJSONParser(url) {
@@ -20,7 +21,7 @@ async function app() {
   await monJSONParser("task.json");
   createDOMHeader();
   createDOMUserInterface();
-  createDOMListTask();
+  printListTask(currentFilter);
 }
 
 function createDOMHeader() {
@@ -121,11 +122,21 @@ function addRowTaskInfo(i, index, titre, date, statut) {
   const td1 = document.createElement("td");
   td1.innerHTML = titre;
   const td2 = document.createElement("td");
-  td2.innerHTML = date;
+  td2.innerHTML = FormatDate(date);
   const td3 = document.createElement("td");
   td3.innerHTML = statut;
   const td4 = document.createElement("td");
   td4.style.width = "30px";
+  const btnComplete = document.createElement("button");
+  btnComplete.setAttribute("type", "button");
+  btnComplete.setAttribute("class", "buttonIcon");
+  btnComplete.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 448 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#1c2933" d="M64 80c-8.8 0-16 7.2-16 16l0 320c0 8.8 7.2 16 16 16l320 0c8.8 0 16-7.2 16-16l0-320c0-8.8-7.2-16-16-16L64 80zM0 96C0 60.7 28.7 32 64 32l320 0c35.3 0 64 28.7 64 64l0 320c0 35.3-28.7 64-64 64L64 480c-35.3 0-64-28.7-64-64L0 96zM337 209L209 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L303 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z"/></svg>`;
+  btnComplete.setAttribute(
+    "onclick",
+    `event.stopPropagation(); completeTask(${i})`
+  );
+  const td5 = document.createElement("td");
+  td5.style.width = "30px";
   const btnModif = document.createElement("button");
   btnModif.setAttribute("type", "button");
   btnModif.setAttribute("class", "buttonIcon");
@@ -134,22 +145,26 @@ function addRowTaskInfo(i, index, titre, date, statut) {
     "onclick",
     `event.stopPropagation(); modalUpdateTask(${i})`
   );
-  const td5 = document.createElement("td");
-  td5.style.width = "30px";
+  const td6 = document.createElement("td");
+  td6.style.width = "30px";
   const btnSuppr = document.createElement("button");
   btnSuppr.setAttribute("type", "button");
   btnSuppr.setAttribute("class", "buttonIcon");
-  btnSuppr.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="20" width="17.5" viewBox="0 0 448 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#1c2933" d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0L284.2 0c12.1 0 23.2 6.8 28.6 17.7L320 32l96 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 96C14.3 96 0 81.7 0 64S14.3 32 32 32l96 0 7.2-14.3zM32 128l384 0 0 320c0 35.3-28.7 64-64 64L96 512c-35.3 0-64-28.7-64-64l0-320zm96 64c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16z"/></svg>`;
+  btnSuppr.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 448 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#1c2933" d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0L284.2 0c12.1 0 23.2 6.8 28.6 17.7L320 32l96 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 96C14.3 96 0 81.7 0 64S14.3 32 32 32l96 0 7.2-14.3zM32 128l384 0 0 320c0 35.3-28.7 64-64 64L96 512c-35.3 0-64-28.7-64-64l0-320zm96 64c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16z"/></svg>`;
   btnSuppr.setAttribute("onclick", `event.stopPropagation(); supprTask(${i})`);
 
-  td4.appendChild(btnModif);
-  td5.appendChild(btnSuppr);
+  if (taches[i]["statut"] !== "Close") {
+    td4.appendChild(btnComplete);
+  }
+  td5.appendChild(btnModif);
+  td6.appendChild(btnSuppr);
   row.appendChild(th);
   row.appendChild(td1);
   row.appendChild(td2);
   row.appendChild(td3);
   row.appendChild(td4);
   row.appendChild(td5);
+  row.appendChild(td6);
   tableBody.appendChild(row);
 }
 function addRowTaskDescription(index, description) {
@@ -168,6 +183,7 @@ function addRowTaskDescription(index, description) {
 }
 
 function printListTask(filter) {
+  currentFilter = filter;
   if (filter === "Toutes") {
     createDOMListTask();
     return console.log("Affiche les Tâches de statut: Toutes");
@@ -230,9 +246,15 @@ function modalUpdateTask(index) {
     openModalUpdateTask(index);
   }
 }
+
+function completeTask(index) {
+  taches[index]["statut"] = "Close";
+  updateJSON(taches);
+  printListTask(currentFilter);
+}
 function supprTask(index) {
   taches.splice(index, 1);
-  createDOMListTask();
+  printListTask(currentFilter);
 }
 
 // Creation de la fenetre modal contenant le formulaire d'ajout ou suppression de tache
@@ -304,8 +326,7 @@ function openModalCreateTask() {
     function (event) {
       event.preventDefault();
       const titre = document.getElementById("inputCreateTitre").value;
-      let date = document.getElementById("inputCreateDate").value;
-      date = FormatDate(date);
+      const date = document.getElementById("inputCreateDate").value;
       const description = document.getElementById(
         "inputCreateDescription"
       ).value;
@@ -314,7 +335,7 @@ function openModalCreateTask() {
       sortByDate(taches);
       updateJSON(taches);
 
-      createDOMListTask();
+      printListTask(currentFilter);
       closeModal("Create");
     },
     { once: true }
@@ -327,7 +348,7 @@ function openModalUpdateTask(index) {
   const titre = document.getElementById("inputUpdateTitre");
   titre.value = taches[index]["title"];
   const date = document.getElementById("inputUpdateDate");
-  date.value = FormatDateTiret(taches[index]["date"]);
+  date.value = taches[index]["date"];
   const description = document.getElementById("inputUpdateDescription");
   description.innerHTML = taches[index]["description"];
 
@@ -336,11 +357,11 @@ function openModalUpdateTask(index) {
     function (event) {
       event.preventDefault();
       taches[index]["title"] = titre.value;
-      taches[index]["date"] = FormatDate(date.value);
+      taches[index]["date"] = date.value;
       taches[index]["description"] = description.value;
       updateJSON(taches);
 
-      createDOMListTask();
+      printListTask(currentFilter);
       closeModal("Update");
     },
     { once: true }
@@ -354,12 +375,12 @@ function closeModal(action) {
 }
 
 // Tri du tableau
-function sortByDate(array) {
+function sortByDate(array, fromNew = true) {
   for (let i = 1; i < array.length; i++) {
     for (let j = array.length - 1; j >= i; j--) {
       if (
-        FormatDateTiret(array[j - 1]["date"]) >
-        FormatDateTiret(array[j]["date"])
+        (fromNew && array[j - 1]["date"] > array[j]["date"]) ||
+        (!fromNew && array[j - 1]["date"] < array[j]["date"])
       ) {
         swap(array, j, j - 1);
       }
