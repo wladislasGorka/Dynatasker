7045e6a0c1a53c13fbfea0460131fa99700e6d7f
wladislasGorka <wladislas.gorka@gmail.com>
2024-11-02 16:04:31 +0100

    Fonction de tri par date (lors de l'ajout d'une tache)

--- script.js
+++ script.js
@@ -36,10 +36,11 @@ function createDOMHeader() {
 function createDOMUserInterface() {
   const container = document.getElementById("interfaceContainer");
   // Bouton  pour ouvrir le modal de création
+  container.innerHTML = ``;
   const createTask = document.createElement("button");
   createTask.setAttribute("id", "btnCreateTask");
   createTask.setAttribute("type", "button");
-  createTask.innerHTML = "Add";
+  createTask.innerHTML = `Ajouter`;
   createTask.setAttribute("onclick", "modalCreateTask()");
 
   const btnContainer = document.createElement("div");
@@ -124,17 +125,21 @@ function addRowTaskInfo(i, index, titre, date, statut) {
   const td3 = document.createElement("td");
   td3.innerHTML = statut;
   const td4 = document.createElement("td");
+  td4.style.width = "30px";
   const btnModif = document.createElement("button");
   btnModif.setAttribute("type", "button");
-  btnModif.innerHTML = "M";
+  btnModif.setAttribute("class", "buttonIcon");
+  btnModif.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 512 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#1c2933" d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160L0 416c0 53 43 96 96 96l256 0c53 0 96-43 96-96l0-96c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 96c0 17.7-14.3 32-32 32L96 448c-17.7 0-32-14.3-32-32l0-256c0-17.7 14.3-32 32-32l96 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L96 64z"/></svg>`;
   btnModif.setAttribute(
     "onclick",
     `event.stopPropagation(); modalUpdateTask(${i})`
   );
   const td5 = document.createElement("td");
+  td5.style.width = "30px";
   const btnSuppr = document.createElement("button");
   btnSuppr.setAttribute("type", "button");
-  btnSuppr.innerHTML = '<img class="icon" src="trash-can.png">';
+  btnSuppr.setAttribute("class", "buttonIcon");
+  btnSuppr.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="20" width="17.5" viewBox="0 0 448 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#1c2933" d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0L284.2 0c12.1 0 23.2 6.8 28.6 17.7L320 32l96 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 96C14.3 96 0 81.7 0 64S14.3 32 32 32l96 0 7.2-14.3zM32 128l384 0 0 320c0 35.3-28.7 64-64 64L96 512c-35.3 0-64-28.7-64-64l0-320zm96 64c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16z"/></svg>`;
   btnSuppr.setAttribute("onclick", `event.stopPropagation(); supprTask(${i})`);
 
   td4.appendChild(btnModif);
@@ -306,6 +311,7 @@ function openModalCreateTask() {
       ).value;
 
       addTask(titre, date, description);
+      sortByDate(taches);
       updateJSON(taches);
 
       createDOMListTask();
@@ -316,7 +322,6 @@ function openModalCreateTask() {
 
   document.getElementById("modalCreateTask").style.display = "block";
 }
-
 function openModalUpdateTask(index) {
   console.log("Ouverture du modal");
   const titre = document.getElementById("inputUpdateTitre");
@@ -343,10 +348,28 @@ function openModalUpdateTask(index) {
 
   document.getElementById("modalUpdateTask").style.display = "block";
 }
-
 function closeModal(action) {
   console.log("Fermeture du modal");
   document.getElementById("modal" + action + "Task").style.display = "none";
+}
+
+// Tri du tableau
+function sortByDate(array) {
+  for (let i = 1; i < array.length; i++) {
+    for (let j = array.length - 1; j >= i; j--) {
+      if (
+        FormatDateTiret(array[j - 1]["date"]) >
+        FormatDateTiret(array[j]["date"])
+      ) {
+        swap(array, j, j - 1);
+      }
+    }
+  }
+}
+function swap(array, index1, index2) {
+  let temp = array[index1];
+  array[index1] = array[index2];
+  array[index2] = temp;
 }
 
 // fetch
