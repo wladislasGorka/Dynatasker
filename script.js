d6dc1e91c006ec840e5179ef0f5082898e717d9d
wladislasGorka <wladislas.gorka@gmail.com>
2024-11-03 15:34:45 +0100

    Correction de bug lors de l'annulation d'une saisie.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let taches; // Tableau des taches construit à partir du fichier json
 let currentTaskDisplay = ""; // tache dont la description est visible
+let currentTaskUpdate = ""; // tache en cours d'update
 let currentFilter = "Toutes"; // filtre en cours d'utilisation pour l'affichage des tâches
 let sortByDateFromNew = true; // Sens du tri par date
 
@@ -72,11 +73,11 @@ function addTask(title, date, description) {
   //console.log(taches);
   createDOMListTask();
 }
-function addTaskCancel() {
-  console.log("Annulation de la saisie!");
-  document.getElementById("inputTaskTitle").value = "";
-  document.getElementById("inputTaskDate").value = "";
-  document.getElementById("inputTaskDescription").value = "";
+function formClear(formAction) {
+  console.log("Clear Form " + formAction);
+  document.getElementById("input" + formAction + "Titre").value = "";
+  document.getElementById("input" + formAction + "Date").value = "";
+  document.getElementById("input" + formAction + "Description").value = "";
 }
 
 // Remplissage du tableau des Tâches.
@@ -144,7 +145,7 @@ function addRowTaskInfo(i, index, titre, date, statut) {
   btnModif.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 512 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#1c2933" d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160L0 416c0 53 43 96 96 96l256 0c53 0 96-43 96-96l0-96c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 96c0 17.7-14.3 32-32 32L96 448c-17.7 0-32-14.3-32-32l0-256c0-17.7 14.3-32 32-32l96 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L96 64z"/></svg>`;
   btnModif.setAttribute(
     "onclick",
-    `event.stopPropagation(); modalUpdateTask(${i})`
+    `event.stopPropagation(); changeCurrentUpdateTask(${i}); modalUpdateTask(${i})`
   );
   const td6 = document.createElement("td");
   td6.style.width = "30px";
@@ -181,6 +182,10 @@ function addRowTaskDescription(index, description) {
   row.appendChild(th);
   row.appendChild(td);
   tableBody.appendChild(row);
+}
+function changeCurrentUpdateTask(index) {
+  console.log("Update de la tache: " + index);
+  currentTaskUpdate = index;
 }
 
 function printListTask(filter) {
@@ -232,6 +237,7 @@ function modalCreateTask() {
   const modal = document.getElementById("modalCreateTask");
   if (modal === null) {
     createModal("Create");
+    formCreateAddEvent();
     openModalCreateTask();
   } else {
     openModalCreateTask();
@@ -242,9 +248,10 @@ function modalUpdateTask(index) {
   const modal = document.getElementById("modalUpdateTask");
   if (modal === null) {
     createModal("Update");
-    openModalUpdateTask(index);
+    formUpdateAddEvent(index);
+    openModalUpdateTask();
   } else {
-    openModalUpdateTask(index);
+    openModalUpdateTask();
   }
 }
 
@@ -255,6 +262,7 @@ function completeTask(index) {
 }
 function supprTask(index) {
   taches.splice(index, 1);
+  updateJSON(taches);
   printListTask(currentFilter);
 }
 
@@ -291,7 +299,10 @@ function createModal(action) {
   formCancel.setAttribute("id", "formCancel" + action + "Task");
   formCancel.setAttribute("type", "button");
   formCancel.innerHTML = "Annuler";
-  formCancel.setAttribute("onclick", 'closeModal("' + action + '")');
+  formCancel.setAttribute(
+    "onclick",
+    'formClear("' + action + '"), closeModal("' + action + '")'
+  );
 
   form.appendChild(formSubmit);
   form.appendChild(formCancel);
@@ -313,7 +324,8 @@ function createInput(form, action, data, type, placeholder) {
       input.setAttribute("rows", "4"))
     : (input = document.createElement("input"));
   type === "date"
-    ? input.setAttribute("type", "date")
+    ? (input.setAttribute("type", "date"),
+      input.setAttribute("min", new Date().toISOString().slice(0, 10)))
     : input.setAttribute("type", "text");
   input.setAttribute("id", "input" + action + data);
   input.setAttribute("placeholder", placeholder);
@@ -322,13 +334,10 @@ function createInput(form, action, data, type, placeholder) {
   form.appendChild(label);
   form.appendChild(input);
 }
-
-function openModalCreateTask() {
-  console.log("Ouverture du modal");
-
-  document.getElementById("formCreateTask").addEventListener(
-    "submit",
-    function (event) {
+function formCreateAddEvent() {
+  document
+    .getElementById("formCreateTask")
+    .addEventListener("submit", function (event) {
       event.preventDefault();
       const titre = document.getElementById("inputCreateTitre").value;
       const date = document.getElementById("inputCreateDate").value;
@@ -341,37 +350,43 @@ function openModalCreateTask() {
       updateJSON(taches);
 
       printListTask(currentFilter);
+      formClear("Create");
       closeModal("Create");
-    },
-    { once: true }
-  );
-
-  document.getElementById("modalCreateTask").style.display = "block";
+    });
 }
-function openModalUpdateTask(index) {
-  console.log("Ouverture du modal");
-  const titre = document.getElementById("inputUpdateTitre");
-  titre.value = taches[index]["title"];
-  const date = document.getElementById("inputUpdateDate");
-  date.value = taches[index]["date"];
-  const description = document.getElementById("inputUpdateDescription");
-  description.value = taches[index]["description"];
+function formUpdateAddEvent() {
+  // const titre = document.getElementById("inputUpdateTitre");
+  // titre.value = taches[index]["title"];
+  // const date = document.getElementById("inputUpdateDate");
+  // date.value = taches[index]["date"];
+  // const description = document.getElementById("inputUpdateDescription");
+  // description.value = taches[index]["description"];
 
-  document.getElementById("formUpdateTask").addEventListener(
-    "submit",
-    function (event) {
+  document
+    .getElementById("formUpdateTask")
+    .addEventListener("submit", function (event) {
       event.preventDefault();
-      taches[index]["title"] = titre.value;
-      taches[index]["date"] = date.value;
-      taches[index]["description"] = description.value;
+      taches[currentTaskUpdate]["title"] =
+        document.getElementById("inputUpdateTitre").value;
+      taches[currentTaskUpdate]["date"] =
+        document.getElementById("inputUpdateDate").value;
+      taches[currentTaskUpdate]["description"] = document.getElementById(
+        "inputUpdateDescription"
+      ).value;
       updateJSON(taches);
 
       printListTask(currentFilter);
+      formClear("Update");
       closeModal("Update");
-    },
-    { once: true }
-  );
+    });
+}
 
+function openModalCreateTask() {
+  console.log("Ouverture du modal");
+  document.getElementById("modalCreateTask").style.display = "block";
+}
+function openModalUpdateTask() {
+  console.log("Ouverture du modal");
   document.getElementById("modalUpdateTask").style.display = "block";
 }
 function closeModal(action) {
